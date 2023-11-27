d2803dd6b9922d645b1cd22f4f51ae30d2393bbf
Martín Pereira <veridicotp22@gmail.com>
2023-11-27 18:44:17 -0300

    Funcion get, post y delete on

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
   //Mostrar el usuario con id=x
-  document.querySelector('#btnGet1').addEventListener('click', obtenerDatos);
+  document.querySelector('#btnGet1').addEventListener('click', getDatos);
   //Agregar nuevo usuario
-  document.querySelector('#btnPost').addEventListener('click', ingresarDato);
+  document.querySelector('#btnPost').addEventListener('click', postDato);
   //Eliminar Usuario
   document.getElementById('btnDelete').addEventListener('click', deleteUser);
 });
+
 //Función GET by ID
-function obtenerDatos() {
+function getDatos() {
   let id = document.getElementById('inputGet1Id').value;
-  //console.log(id);
-  if (id != null) {
+  if (id !== "") {
     fetch(`https://65427c36ad8044116ed3720a.mockapi.io/users/${id}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data);
         mostrarDatos(data);
       })
       .catch(err => {
         console.error('There was an error', err);
       });
   } else {
-    fetch(`https://65427c36ad8044116ed3720a.mockapi.io/users`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        //mostrarDatos(data);
-      })
-      .catch(err => {
-        console.error('There was an error', err);
-      });
+    getAll();
   }
 }
 //Mostrar datos de usuario si ID=x
 function mostrarDatos(data) {
   let container = document.querySelector('#results');
-
-  container.innerHTML = `<div><p>ID: ${data.id}</p> <p>NAME: ${data.name}</p> <p>LAST NAME: ${data.lastname}</p></div>`;
+  container.innerHTML = "";
+  container.innerHTML = `<div><p>id: ${data.id}</p><p>name: ${data.name}</p><p>lastname: ${data.lastname}</p></div>`;
 }
 //Mostrar datos de usuarios si ID=null
 function showAll(data) {
   let container = document.getElementById('results');
+  container.innerHTML = "";
   for (let one of data) {
     container.innerHTML += `
     <div>
-      <p>ID: ${one.id}</p>
-      <p>NAME: ${one.name}</p>
-      <p>LAST NAME: ${one.lastname}</p>
+      <p>id: ${one.id}</p>
+      <p>name: ${one.name}</p>
+      <p>lastname: ${one.lastname}</p>
     </div>
     `;
   }
 }
 //Funcion POST
-function ingresarDato() {
+function postDato() {
   let nombre = document.querySelector('#inputPostNombre').value;
   let apellido = document.querySelector('#inputPostApellido').value;
   let datos = {
@@ -65,20 +57,39 @@ function ingresarDato() {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(datos),
-  });
+  })
+  .then(response=> response.json())
+  .then(data=>{
+    getAll(data);
+  })
+  .catch(error=> {
+    alert(error);
+  })
 }
 //Funcion DELETE
 function deleteUser() {
-  let entrada = document.getElementById('inputDelete').value;
-  let URL = `https://65427c36ad8044116ed3720a.mockapi.io/users/${entrada}`;
-
+  let entrada = document.getElementById('inputDelete');
+  let URL = `https://65427c36ad8044116ed3720a.mockapi.io/users/${entrada.value}`;
   fetch(URL, { method: 'DELETE' })
-    .then(response => {
-      console.log('Se han cargado los datos', response);
+    .then(response => response.json())
+    .then(data=> {
+      entrada.innerHTML = null;
+      getAll();
     })
     .catch(error => {
       console.error('Error al enviar los datos', error);
     });
 }
 //Función PUT
-function editUser() {}
+function editUser() {};
+//Get Todos
+function getAll(){
+  fetch(`https://65427c36ad8044116ed3720a.mockapi.io/users`)
+  .then(response => response.json())
+  .then(data => {
+    showAll(data);
+  })
+  .catch(err => {
+    console.error('There was an error', err);
+  });
+}
